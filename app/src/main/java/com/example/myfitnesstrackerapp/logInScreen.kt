241626f9cbb6bbc0f73bcf3833017a2c5631b20a
Joy-kitet <debrajoykitet@gmail.com>
2025-03-09 04:06:10 -0700

    LoginPage

---
+++ app/src/main/java/com/example/myfitnesstrackerapp/logInScreen.kt
@@ -0,0 +1,65 @@
+package com.example.myfitnesstrackerapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun logInScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.imagelogo), contentDescription = "Login image",
+            modifier = Modifier.size(200.dp))
+        Text(text = "Welcome back", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = "Login to your account")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = "", onValueChange = {}, label = {
+            Text(text = "Email Address")
+        })
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = "", onValueChange = {}, label = {
+            Text(text = "Password")
+        })
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { }) {
+            Text(text = "Login")
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(text = "Forgot password?", modifier = Modifier.clickable {
+
+        })
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(text = "or sign in with")
+    }
+
+}
